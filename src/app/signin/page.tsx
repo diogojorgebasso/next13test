130d8c5b58bbc08ba737ac2ee0880f5ae8a96fde
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-04-22 15:37:56 -0300

    change to prettier
    Signed-off-by: Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,47 +1,73 @@
 //renders code in client instead of Server
-'use client'
+"use client";
 
 import React, { useCallback } from "react";
 import signIn from "/firebase/auth/signin";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 function Page() {
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const router = useRouter()
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const router = useRouter();
 
-  const handleClickEmail = useCallback((e: React.FormEvent<HTMLInputElement>) => { setEmail(e.target.value) }, [])
-  const handleClickPassword = useCallback((e: React.FormEvent<HTMLInputElement>) => { setPassword(e.target.value) }, [])
+  const handleClickEmail = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setEmail(e.target.value);
+    },
+    []
+  );
+  const handleClickPassword = useCallback(
+    (e: React.FormEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
+    []
+  );
 
   const handleForm = async (event: React.FormEvent<HTMLElement>) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const { result, error } = await signIn(email, password);
 
     if (error) {
-      return console.log(error)
+      return console.log(error);
     }
 
     // else successful
-    console.log(result)
-    return router.push("/admin")
-  }
-  return (<div className="wrapper">
-    <div className="form-wrapper">
-      <h1 className="mt-60 mb-30">Login</h1>
-      <form onSubmit={handleForm} className="form">
-        <label htmlFor="email">
-          <p>Email</p>
-          <input onChange={handleClickEmail} required type="email" name="email" id="email" placeholder="example@mail.com" />
-        </label>
-        <label htmlFor="password">
-          <p>Senha</p>
-          <input onChange={handleClickPassword} required type="password" name="password" id="password" placeholder="password" />
-        </label>
-        <button type="submit">Preenchi</button>
-      </form>
+    console.log(result);
+    return router.push("/admin");
+  };
+  return (
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1 className="mt-60 mb-30">Login</h1>
+        <form onSubmit={handleForm} className="form">
+          <label htmlFor="email">
+            <p>Email</p>
+            <input
+              onChange={handleClickEmail}
+              required
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@mail.com"
+            />
+          </label>
+          <label htmlFor="password">
+            <p>Senha</p>
+            <input
+              onChange={handleClickPassword}
+              required
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+            />
+          </label>
+          <button type="submit">Preenchi</button>
+        </form>
+      </div>
     </div>
-  </div>);
+  );
 }
 
 export default Page;
